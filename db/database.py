836b9abd4a1846a44278847b84604871ee87a5af
unknown <Aprendiz@BOGDFPCGMS4141.FormacionSena.RED>
2025-05-09 11:13:52 -0500

    pyshopy

--- db/database.py
+++ db/database.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine #para importar funciones de una dependencia
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALDHEMY_DATEBASE_URL='mariadb://root@admin/localhost:3315/py-shopy'
+SQLALDHEMY_DATEBASE_URL= 'mariadb://root@admin/localhost:3315/py-shopy'
